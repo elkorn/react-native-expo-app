5647124399060d8242a9efbddd2bc8aeeee4a266
elkorn <helluinster@gmail.com>
2020-08-29 18:35:17 +0200

    fix(service): remove references to unused dependencies

--- packages/service/src/Server.ts
+++ packages/service/src/Server.ts
@@ -1,14 +1,12 @@
-import cookieParser from 'cookie-parser';
-import morgan from 'morgan';
-import path from 'path';
-import helmet from 'helmet';
+import morgan from "morgan";
+import helmet from "helmet";
 
-import express, { Request, Response, NextFunction } from 'express';
-import { BAD_REQUEST } from 'http-status-codes';
-import 'express-async-errors';
+import express, { Request, Response, NextFunction } from "express";
+import { BAD_REQUEST } from "http-status-codes";
+import "express-async-errors";
 
-import { router } from './routes';
-import { logger } from './shared/Logger';
+import { router } from "./routes";
+import { logger } from "./shared/Logger";
 
 // Init express
 export const app = express();
@@ -17,17 +15,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Show routes called in console during development
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Security
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   app.use(helmet());
 }
 
 // Add APIs
-app.use('/', router);
+app.use("/", router);
 
 // Print API errors
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
