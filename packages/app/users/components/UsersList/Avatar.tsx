618b85cf81aa3a7d2d39e4be2e5760b436f04472
elkorn <helluinster@gmail.com>
2020-08-30 18:26:07 +0200

    feat(app/avatar): add a loading fade-in animation

--- packages/app/users/components/UsersList/Avatar.tsx
+++ packages/app/users/components/UsersList/Avatar.tsx
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
-import { Image, StyleSheet, View, ActivityIndicator } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { ActivityIndicator, Animated, StyleSheet, View } from "react-native";
 import { LoadingState } from "../../../shared/types";
 
 export default function Avatar({ src }: AvatarProps) {
   const [loading, setLoading] = useState(LoadingState.Idle);
+  const fadeValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    if (loading === LoadingState.Success) {
+      Animated.spring(fadeValue, {
+        toValue: 1,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [loading]);
 
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.image}
+      <Animated.Image
+        style={{ ...styles.image, opacity: fadeValue }}
         onLoadStart={() => setLoading(LoadingState.Loading)}
         onLoad={() => setLoading(LoadingState.Success)}
         source={{
